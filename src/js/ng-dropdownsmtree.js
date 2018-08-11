e53a7499fd164c850ab1a4eba0a52635e26df776
Asmaa <a.elbasioni@gmail.com>
2018-08-12 00:24:45 +0200

    feat: Move the directive into separate module
    -  Fix some es6 syntax
    -  In the search input, change oninput function to ng-change function

--- src/js/ng-dropdownsmtree.js
+++ src/js/ng-dropdownsmtree.js
@@ -1,5 +1,7 @@
-//angular.module( "ngDropdownsmtree", [])
-app.directive('ngDropdownsmtree', function ($filter) {
+'use strict';
+
+angular.module('ngDropdownsmtree', [])
+.directive('ngDropdownsmtree', ['$filter', ($filter)=> {
     return {
         // can be used as attribute(A) or element(E)
         restrict: 'AE',
@@ -19,15 +21,15 @@ app.directive('ngDropdownsmtree', function ($filter) {
 
 
         // the markup this directive generates
-        template: function (iElement, iAttrs) {
+        template: (iElement, iAttrs) => {
 
-            var html = '<div class="btn-group" style="width:100%">' +
+            const html = '<div class="btn-group" style="width:100%">' +
                             '<button type="button" class="btn dropdown-toggle form-control ' + iAttrs.controlClass + '" data-ng-class="{\'disabled\':disabled == true}"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >' +
                                 '<i class="glyphicon glyphicon-triangle-bottom pull-right" style="margin-top:3px"></i>' +
                                  '{{selectedsubjectdesc}}' +
                             '</button>            ' +
                             '<ul class="dropdown-menu custom-dropdown-menu" >' +
-                                '<li ><input type="text" placeholder="' + iAttrs.searchPlaceholder + '" class="form-control"  oninput="highlightContainers(this)" data-ng-model="SearchSubjectFLTR" data-ng-init="SearchSubjectFLTR =\'\'" /></li>' +
+                                '<li ><input type="text" placeholder="' + iAttrs.searchPlaceholder + '" class="form-control"  ng-change="highlightContainers()" data-ng-model="SearchSubjectFLTR" data-ng-init="SearchSubjectFLTR =\'\'" /></li>' +
                                 '<li data-ng-repeat="item in parentArray" data-src="{{item[\''+iAttrs.parentArrayItemIdField+'\']}}">' +
                                     '<span  onclick="event.stopPropagation();" data-ng-click="showSecondLevel($event)" ><span class="glyphicon glyphicon-plus"></span> {{item[parentArrayItemDisplayField]}}</span>' +
                                     '<div class="content" style="margin-left: 14px; overflow: hidden">' +
@@ -47,59 +49,49 @@ app.directive('ngDropdownsmtree', function ($filter) {
 
         // this function is called on each ng-Twolevelddl instance initialisation
         // we just declare what we need in the above template
-        link: function (scope, iElement, iAttrs, ctrl) {
+        link: (scope, iElement, iAttrs, ctrl) => {
 
-            ctrl.$render = function () {
+            ctrl.$render = () => {
                 if(ctrl.$viewValue)
                   scope.selectedsubjectdesc = ctrl.$viewValue.Description;
                 else
                   scope.selectedsubjectdesc = scope.titlePlaceholder;
             };
-      
-            highlightContainers = function (event) {
+            
+            scope.highlightContainers = () => {
 
-                if(!event)
-                    return;
-                
-                var val = $(event).val();
-                
                 //remove previous heighlights
-                var elm = $(event).closest('ul');
+                const elm = $(iElement).find('ul');
                 elm.find(" >li > span.has-data").removeClass('has-data');
 
                 //height the new elements that contains the new search test
-                if (val.length > 0) {
-                    var _arr = $filter('filter', scope.childArraysItemSearchField)(scope.childArrays, val);
-                    console.log("_arr1",_arr)
-                    for (var i = 0; i < _arr.length; i++) {
+                if (scope.SearchSubjectFLTR.length > 0) {
+                    const _arr = $filter('filter', scope.childArraysItemSearchField)(scope.childArrays, scope.SearchSubjectFLTR);
+                    for (let i = 0; i < _arr.length; i++) {
                         if (_arr[i][iAttrs.childArraysItemParentId] === null)
                             elm.find("[data-src=''] >span").addClass('has-data');                        
                         else
-                            elm.find("[data-src="+_arr[i][iAttrs.childArraysItemParentId]+"] >span").addClass('has-data');
+                            elm.find(`[data-src=${_arr[i][iAttrs.childArraysItemParentId]}] >span`).addClass('has-data');
                     }
                 }
             };
 
-            scope.showSecondLevel = function (event) {
+            scope.showSecondLevel = (event) => {
 
-                var clickedItem = $(event.currentTarget || event.srcElement).closest('li');
+                const clickedItem = $(event.currentTarget || event.srcElement).closest('li');
                 clickedItem.find('.content').slideToggle(500);
                 clickedItem.find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
 
             };
 
-            scope.setselecteditem = function (sb) {
+            scope.setselecteditem = (sb) => {
                 ctrl.$setViewValue(sb);
                 ctrl.$render();
             };
 
-
-
-
         }
 
-
     };
-});
+}]);
 
 
